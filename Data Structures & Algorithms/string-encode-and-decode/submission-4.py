a505e8d9dd6ee043e17762baa2021301e00dfaab
ayoub elebbar <ayoubelebbar09@gmail.com>
2026-07-28 09:49:24 +0100

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        s = ":.".join(strs) + ":"
+        print(s)
+        return s
+
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return []
+        strs = []
+        for elem in s.rsplit("."):
+            strs.append(elem[:-1])
+        return strs
